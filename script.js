92db9371f1ff8574b682cf5f46541be4065b0c51
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-08-30 17:48:14 +0100

    added js to display information about countries on the page

--- script.js
+++ script.js
@@ -13,7 +13,37 @@ const SetUp = () => {
         .then((jsonData) => {
             allCountries = jsonData;
             console.log(allCountries);
+            addCountryToList();
         });
+}
+
+const addCountryToList = () => {
+    for (let i = 0; i < allCountries.length; i++){
+        const countryFromArray = allCountries[i];
+
+        const countriesList = document.querySelector("#countries-list")
+
+        console.log(countryFromArray);
+        const countryListItem = document.createElement("li");
+        const name = document.createElement("h4");
+        const continent = document.createElement("p");
+        const population = document.createElement("p");
+
+        name.innerText = countryFromArray.name.common;
+        countryListItem.appendChild(name);
+
+        continent.innerText = countryFromArray.continents;
+        countryListItem.appendChild(continent);
+
+        population.innerText = countryFromArray.population;
+        countryListItem.appendChild(population);
+
+        countriesList.appendChild(countryListItem);
+
+        console.log(countriesList);
+        console.log(countryListItem);
+
+    }
 }
 
 SetUp();
